16459a8d4484f576067b831e20bd98a65199368a
Sam James <samjojames@gmail.com>
2022-10-25 20:23:11 +0100

    Add database reset scripts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
-  signup() {
-    return 'I am signed up';
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException();
+        }
+      }
+    }
   }
 
-  signin() {
-    return 'I am signed in';
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw NotFoundException;
+    const hash = await argon.hash(dto.password);
+    if (user.hash !== hash) {
+      throw new ForbiddenException();
+    }
+    return user;
   }
 }
